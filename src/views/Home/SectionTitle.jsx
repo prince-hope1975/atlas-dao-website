800952fa02cfd40fd6db8eb097487e41db1c425d
iamagf <102500749+cosmo-jorin@users.noreply.github.com>
2022-05-15 11:45:45 -0500

    Div reordered to give spacing

--- src/views/Home/SectionTitle.jsx
+++ src/views/Home/SectionTitle.jsx
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 import { Parallax } from "react-scroll-parallax";
 
 const SectionTitle = ({data}) => {
-  return  <Parallax 
-            opacity= {[0, 1]}
-            easing={[.76,1.17,.52,1]}
-            >
-              <div className="homepage-title">{data.title}</div>
-          </Parallax>;
+  return  <div className="homepage-title">
+            <Parallax 
+              opacity= {[0, 1]}
+              easing={[.76,1.17,.52,1]}
+              >
+                {data.title}
+            </Parallax>
+          </div>;
 };
 
 SectionTitle.propTypes = { data: PropTypes.object };
