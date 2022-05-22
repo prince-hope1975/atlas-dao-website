cf63e33d348a44daeb0843564590b75a73866237
iamagf <102500749+cosmo-jorin@users.noreply.github.com>
2022-05-22 14:55:13 -0500

    Update App.jsx
    Swap home to sky monks. Requires validator and home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 
 import "../sass/app.scss";
 import Home from "../views/Home/Home";
+import SkyMonks from "../views/SkyMonks/SkyMonks";
 import Roadmap from "../views/Roadmap/Roadmap";
 import Gallery from "../views/Gallery/Gallery";
 
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<BaseLayout />}>
           <Route path="" element={<Home />} />
+          <Route path="/sky-monks" element={<SkyMonks />} />
           <Route path="/roadmap" element={<Roadmap />} />
           <Route path="/gallery" element={<Gallery />} />
 
