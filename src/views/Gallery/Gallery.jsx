6f19df61aa79466ed0c09c70b20952b2ecb7195d
iamagf <102500749+cosmo-jorin@users.noreply.github.com>
2022-06-18 18:53:09 -0500

    Gallery addition

--- src/views/Gallery/Gallery.jsx
+++ src/views/Gallery/Gallery.jsx
@@ -1,33 +1,60 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { Component } from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import PropTypes from 'prop-types';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  ClearRefinements,
+  RefinementList,
+} from 'react-instantsearch-dom';
 
-import SimpleImageComponent from "./SimpleImageComponent/SimpleImageComponent";
-import ParalaxSectionTitle from "../../components/SectionTitle/ParalaxSectionTitle";
+const searchClient = algoliasearch(
+  'YZO5WZ7G6C', // Key App
+  '1ec2ab36e6b367aabed0d3c493ac4006' // Key API db
+);
 
 const Gallery = () => {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ParalaxSectionTitle title="The Gallery" />
-      <div className="team-info">
-        Coming Soon
-      </div>
-      {/* <>
-        {team.map((item, index) => (
-          <SimpleImageComponent key={index} data={item} />
-        ))}
-      </> */}
-    </Grid>
+    <div className="gallery">
+    <InstantSearch searchClient={searchClient} indexName="atlasdao">
+        <h2>Search by parameters</h2>
+        <div className=''>
+          <div className='refinement-block'>
+            <h3>Country</h3>
+            <RefinementList attribute={"hand"}/>
+          </div>
+          <div className='refinement-block'>
+            <h3>Hold</h3>
+            <RefinementList attribute={"hold"}/>
+          </div>
+        </div>
+          
+        <SearchBox 
+        searchAsYouType={true}/>
+          <Hits hitComponent={HitComponent}/>
+          <Pagination/>
+      </InstantSearch>
+    </div>
   );
 };
 
-Gallery.propTypes = {};
+
+function HitComponent(props) {
+  const url = props.hit.url;
+  const id = props.hit.id;
+
+  return (
+    <div className="HitComponent">
+      <img src={url}/>
+      <p>Sky Monk #{id}</p>
+    </div>
+  );
+};
+
+HitComponent.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
 
 export default Gallery;
