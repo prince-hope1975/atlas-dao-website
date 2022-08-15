0b08d33ea3244d3783da1daf2a9c0b2f5df6b695
iamagf <agf0710@gmail.com>
2022-08-15 00:25:31 -0500

    Header fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,28 +3,23 @@ import PropTypes from "prop-types";
 import {
   AppBar,
   CssBaseline,
-  Slide,
   Toolbar,
   useMediaQuery,
   useScrollTrigger,
   useTheme,
 } from "@mui/material";
-import { BlurCircular, Widgets } from "@mui/icons-material";
 import MenuEntry from "./MenuEntry";
 import SideMenu from "./SideMenu";
 import logo from "../../images/logo.png";
 
 const Header = ({ window }) => {
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  });
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const menuEntries = [
     { label: "Home", url: "/" },
-    { label: "Roadmap", url: "/roadmap" },
+    // { label: "Roadmap", url: "/roadmap" },
     { label: "Sky Monks", url: "/sky-monks" },
     { label: "Validator", url: "/validator" },
     { label: "Gallery", url: "/gallery" },
@@ -32,31 +27,34 @@ const Header = ({ window }) => {
   ];
 
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      <AppBar
-        // enableColorOnDark
-        sx={{
-          backgroundColor: "transparent",
-          backgroundImage: "none",
-        }}
-      >
-        <CssBaseline />
-        <Toolbar>
-          <div style={{ flexGrow: 1 }}>
-            <img src={logo} style={{width:"5vw"}}/>
-          </div>
-          {isMobile ? (
-            <SideMenu entries={menuEntries} />
-          ) : (
-            <>
-              {menuEntries.map((entry) => {
-                return <MenuEntry entry={entry} key={entry.label} />;
-              })}
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Slide>
+
+    <AppBar
+      // enableColorOnDark
+      sx={{
+        backgroundColor: "transparent",
+        backgroundImage: "none",
+      }}
+
+      position="fixed"
+
+    >
+      <CssBaseline />
+      <Toolbar>
+        <div style={{ flexGrow: 1 }}>
+          <img src={logo} style={{ width: "5vw" }} />
+        </div>
+        {isMobile ? (
+          <SideMenu entries={menuEntries} />
+        ) : (
+          <>
+            {menuEntries.map((entry) => {
+              return <MenuEntry entry={entry} key={entry.label} />;
+            })}
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+
   );
 };
 
